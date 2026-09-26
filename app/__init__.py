acfff58b35aa8cabe11af92f081c5fb6d1e4cc00
JoyYueLyu <lvyuejoy@gmail.com>
2026-09-26 14:14:58 +1200

    log in out

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,7 @@ def create_app():
     from app.routes.handover import handover_bp
     app.register_blueprint(handover_bp)
 
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
+
     return app
